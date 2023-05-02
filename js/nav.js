706135430d044b984f6bf24d831f80c6369e783b
PotterPlaysGames <105192939+PotterPlaysGames@users.noreply.github.com>
2023-05-02 19:53:54 -0400

    Altered

--- js/nav.js
+++ js/nav.js
@@ -8,8 +8,8 @@ const navBarNotLoggedIn= `
 //Navigation bar for when the user is logged in
 const navBarLoggedIn= `
    <ul id="nav">
-    <li class="navli"><a class="navA" href="./index.html">Home</a></li
-    <li class="navli" style="float:right"><a class="navA" href="./Logout.html">Logout!</a></li>
+    <li class="navli"><a class="navA" href="./index.html">Home</a></li>
+    <li class="navli" style="float:right"><a class="navA" href="./logout.html">Logout!</a></li>
    </ul>`
 
 //Checks if the user is logged in
