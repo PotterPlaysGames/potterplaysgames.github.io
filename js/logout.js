5903a2c062e9bbf8364d6ff1bb4c7f2a6af0b25a
PotterPlaysGames <105192939+PotterPlaysGames@users.noreply.github.com>
2023-05-03 23:49:43 +0000

    Delete js directory

--- js/logout.js
+++
@@ -1,8 +0,0 @@
-let lOut = document.getElementById("logoutButton");
-
-lOut.addEventListener("click", (e) => {
-    e.preventDefault();
-    localStorage.clear();
-    alert("Logout Successful!")
-    location.href = "./login.html"
-})
