17ee8fc77979ddf22bcc46fd1da940f94bc81fc6
Aidan Potter <s_appotter@pennwest.edu>
2023-08-01 16:54:51 -0400

    some stuff added

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,5 +1,13 @@
-//Navigation bar for when the user is not logged in
-const navBarNotLoggedIn= `
+let auth0 = null;
+
+window.onload = async () => {
+    auth0 = await window.createAuth0Client({
+        domain: 'dev-difkemcsbh4bjh0k.us.auth0.com',
+        client_id: 'nZbC5fLSuxF7XsbZxKleoDJfhTbXEIiJ'
+    });
+
+    //Navigation bar for when the user is not logged in
+    const navBarNotLoggedIn= `
 	    <ul>
 			<li><a href="index.html">Home</a></li>
 			<li>
@@ -8,12 +16,12 @@ const navBarNotLoggedIn= `
 						<li><a href="./contact.html">Contact</a></li>
 					</ul>
 			</li>
-				<li><a href="./login.html" class="button">Login</a></li>
+				<li><a href="#" id="lbutton" class="button">Login</a></li>
 				<li><a href="#" class="button">Sign Up</a></li>
 		</ul>`
 
-//Navigation bar for when the user is logged in
-const navBarLoggedIn= `
+    //Navigation bar for when the user is logged in
+    const navBarLoggedIn= `
 	    <ul>
 			<li><a href="./index.html">Home</a></li>
 			<li>
@@ -26,37 +34,35 @@ const navBarLoggedIn= `
 				<li><a href="#" class="button" id="logout">Logout!</a></li>
 		</ul>`
 
-//Checks if the user is logged in
-function checkLogin() {
-    // Checks if Login is set to true in the localStorage
-    if (localStorage.getItem("Login") === "true") {
-        // If true, it sets the nav bar as logged in
-        document.getElementById("nav").insertAdjacentHTML("afterbegin", navBarLoggedIn);
-        // Adds event listener to the logout button
-        document.getElementById("logout").addEventListener("click", logout);
-    } else if (localStorage.getItem("Login") === "false") {
-        // If false, it sets the nav bar as not logged in
-        document.getElementById("nav").insertAdjacentHTML("afterbegin", navBarNotLoggedIn);
+    function isAuthenticated() {
+        let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+        return new Date().getTime() < expiresAt;
     }
-}
 
-//this function sets Login to false if Login is null
-function setLoginFalse() {
-    if (localStorage.getItem("Login") == null) {
-        localStorage.setItem("Login", false)
+    function logout() {
+        auth0.logout({
+            returnTo: 'https://ppgmc.org/index.html',
+            clientID: 'nZbC5fLSuxF7XsbZxKleoDJfhTbXEIiJ'
+        });
     }
-}
 
+    //Checks if the user is logged in
+    function checkLogin() {
+        if (isAuthenticated()) {
+            document.getElementById("nav").insertAdjacentHTML("afterbegin", navBarLoggedIn);
+            document.getElementById("logout").addEventListener("click", logout);
+        } else {
+            document.getElementById("nav").insertAdjacentHTML("afterbegin", navBarNotLoggedIn);
+            let loginButton = document.getElementById("lbutton"); // Moved this line here
+            loginButton.addEventListener('click', (e) => {
+                e.preventDefault();
+                auth0.loginWithRedirect();  //Changed this line
+            });
+        }
+    }
 
-// Logs the user out by setting Login to false and redirecting to login page
-function logout() {
-    localStorage.setItem("Login", false);
-    window.location.href = "./login.html";
-}
-
-//calls functions
-setLoginFalse();
-checkLogin();
+    checkLogin();
+};
 
 
 
