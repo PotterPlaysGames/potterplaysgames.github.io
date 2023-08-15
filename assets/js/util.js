2d7d5838a5268929fe575b96337ad6d59fabee60
Aidan Potter <s_appotter@pennwest.edu>
2023-08-15 15:48:10 -0400

    some stuff added

--- assets/js/util.js
+++ assets/js/util.js
@@ -95,10 +95,12 @@
 			}, userConfig);
 
 			// Expand "target" if it's not a jQuery object already.
-				if (typeof config.target != 'jQuery')
+				if (!(config.target instanceof jQuery)) {
 					config.target = $(config.target);
+				}
 
-		// Panel.
+
+			// Panel.
 
 			// Methods.
 				$this._hide = function(event) {
